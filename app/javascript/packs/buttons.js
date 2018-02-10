18de391704f526a27398ec2dd4ee764ce61dd2f5
Clement Meyer <meyclem@gmail.com>
2018-02-10 19:56:10 +0100

    testing on btns

--- app/javascript/packs/buttons.js
+++ app/javascript/packs/buttons.js
@@ -1,32 +1,13 @@
 const app = document.getElementById('app');
-colors = ['materialize-red', 'green']
-color = colors[Math.floor(Math.random() * colors.length)]
-const btn = `<i class="button ${color} medium menu_icon material-icons waves-effect">
-                ac_unit
-              </i>`
-const classes = 'btn-container center-align'
+const btns = document.querySelectorAll('.button')
 
-const div = (classes, btn) => {
-  return `<div class="${classes}">
-    ${btn}
-  </div>`
-}
-const li = `<li>${div(classes, btn).repeat(5)}</li>`
-console.log(li)
-const ul = (line) => {
-  return `<ul>
-    ${line}
-  </ul>`
-}
+btns.forEach((btn) => {
+  btn.addEventListener('click', (event) => {
+    const btn = event.target
+    btn.classList.toggle('idle')
+    btn.classList.toggle('walk')
+  });
+})
 
-app.insertAdjacentHTML('beforeend', ul(li.repeat(5)))
-
-// function appendElement(nb, parent, child) {
-//   var times = nb;
-//   for(var i=0; i < times; i++){
-//     console.log(parent)
-//     parent.insertAdjacentHTML('beforeend', child)
-//   }
-// }
-
-// appendElement(5, ul, li)
+const string = "01100"
+console.log(string[3])
