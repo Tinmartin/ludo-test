fb4dd5c6354ee471baa0455dcbff37dc0be132ba
Clement Meyer <meyclem@gmail.com>
2018-02-12 14:23:26 +0100

    fix/btns

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,44 +7,7 @@
   // To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
   // layout file, like app/views/layouts/application.html.erb
 
-  document.addEventListener("DOMContentLoaded", (event) => {
+  // document.addEventListener("DOMContentLoaded", (event) => {
     // puts your Materialize initializers here!
     // M.toast({html: "Exemple of Materialize component", displayLength: 5000});
-  });
-function refresh() {
-  fetch(url)
-  fetch("https://ludo-test-rails.herokuapp.com/test")
-  .then(response => response.json())
-  .then((data) => {
-    data.forEach(function(trigger) {
-      const trigId = `trig-${trigger.id}`;
-      const btn = document.getElementById(trigId);
-      if (trigger.id === 1) {
-        if (trigger.state === 1) {
-          char.style.backgroundImage = "url('https://res.cloudinary.com/coryandre/image/upload/v1518258450/walk_color_512_uxmwpn.gif')"
-        } else {
-          char.style.backgroundImage = "url('https://res.cloudinary.com/coryandre/image/upload/v1518258433/idle_color_512_vw3eud.gif')"
-        }
-      }
-      if (trigger.state === 1) {
-        btn.classList.add('green')
-        btn.classList.remove('materialize-red')
-      } else {
-        btn.classList.add('materialize-red')
-        btn.classList.remove('green')
-      }
-    });
-  });
-}
-
-const triggers = document.querySelectorAll('.trigger')
-triggers.forEach(function(trigger) {
-  trigger.addEventListener('click', function(event) {
-    trigger.classList.toggle('green');
-    trigger.classList.toggle('materialize-red');
-  });
-});
-
-const char = document.getElementById('char');
-
-setInterval(refresh, 50);
+  // });
--- app/javascript/packs/buttons.js
+++ app/javascript/packs/buttons.js
@@ -4,10 +4,24 @@ const btns = document.querySelectorAll('.button')
 btns.forEach((btn) => {
   btn.addEventListener('click', (event) => {
     const btn = event.target
-    btn.classList.toggle('idle')
-    btn.classList.toggle('walk')
+    toggleBtn(btn)
+    counter = btn.querySelector('.counter')
+    value = parseInt(counter.innerText)
+    if (value < 9) {
+      counter.innerText = value + 1
+    }
   });
 })
 
-const string = "01100"
-console.log(string[3])
+function toggleBtn(btn) {
+  if (btn.classList.contains("dragon-ball") || btn.classList.contains("nuclear")) {
+    btn.classList.toggle('dragon-ball')
+    btn.classList.toggle('nuclear')
+  } else {
+    btn.classList.toggle('idle')
+    btn.classList.toggle('walk')
+  }
+}
+// refresh
+  // if game started
+  // if game finished
